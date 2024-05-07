e9f4b51f225deb9005ede078884876dc940cf860
Tim Fuller <tjfulle@sandia.gov>
2024-05-07 11:36:46 -0600

    increase test timeout multiplier

--- tests/examples.py
+++ tests/examples.py
@@ -67,7 +67,7 @@ def test_enable(capfd, tmp_path):
 def test_timeoutx(tmp_path):
     with working_dir(tmp_path):
         assert nvtest("run", "-w", f"{examples_dir}/timeoutx") == 8
-        assert nvtest("run", "-w", "-l", "test:timeoutx:2.5", f"{examples_dir}/timeoutx") == 0
+        assert nvtest("run", "-w", "-l", "test:timeoutx:5", f"{examples_dir}/timeoutx") == 0
 
 
 def test_vvt(tmp_path):
