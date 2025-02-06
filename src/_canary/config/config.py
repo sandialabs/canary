6609ec79e5282834c9d86e44c38706c3d29323f3
Tim Fuller <tjfulle@sandia.gov>
2025-02-06 05:45:04 -0700

    use default_factory for mutable config variable

--- src/_canary/config/config.py
+++ src/_canary/config/config.py
@@ -167,7 +167,7 @@ class Build:
 @dataclasses.dataclass
 class Batch:
     duration: float = 30 * 60  # 30 minutes
-    default_options: list[str] = []
+    default_options: list[str] = dataclasses.field(default_factory=list)
 
     def update(self, *args: Any, **kwargs: Any) -> None:
         if len(args) > 1:
