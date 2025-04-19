c39d1b8fb5e6278582740135a2195a1111bffbc0
Matthew Mosby <mdmosby@sandia.gov>
2025-04-18 20:21:22 -0700

    teach `canary log --error <pathspec>` to display stderr for the specified test case if it exists

--- src/_canary/plugins/subcommands/log.py
+++ src/_canary/plugins/subcommands/log.py
@@ -26,6 +26,7 @@ class Log(CanarySubcommand):
     def setup_parser(self, parser: "Parser") -> None:
         parser.epilog = self.in_session_note()
         parser.add_argument("testspec", help="Test name, /TEST_ID, or ^BATCH_ID")
+        parser.add_argument("--error", default=False, action="store_true", help="Display test stderr if it exists")
 
     def execute(self, args: argparse.Namespace) -> int:
         import pydoc
@@ -36,31 +37,30 @@ class Log(CanarySubcommand):
         file: str
         if args.testspec.startswith("/"):
             case = testcase_from_id(args.testspec[1:])
-            file = case.logfile()
+            file = case.logfile() if not args.error else case.stderr()
             if not os.path.isfile(file):
-                file = case.logfile(stage="run")
-            if not os.path.isfile(file):
-                raise ValueError(f"{file}: no such file")
-            print(f"{file}:")
-            pydoc.pager(open(file).read())
+                file = case.logfile(stage="run") if not args.error else case.stderr(stage="run")
+            display_file(file)
             return 0
 
         if args.testspec.startswith("^"):
             file = TestBatch.logfile(args.testspec[1:])
-            print(f"{file}:")
-            if not os.path.isfile(file):
-                raise ValueError(f"{file}: no such file")
-            pydoc.pager(open(file).read())
+            display_file(pydoc, file)
             return 0
 
         session = load_session()
         for case in session.cases:
             if case.matches(args.testspec):
-                file = case.logfile()
-                if not os.path.isfile(file):
-                    raise ValueError(f"{file}: no such file")
-                print(f"{file}:")
-                pydoc.pager(open(file).read())
+                file = case.logfile() if not args.error else case.stderr()
+                display_file(file)
                 return 0
 
         raise ValueError(f"{args.testspec}: no matching test found in {session.work_tree}")
+
+
+def display_file(file: str) -> None:
+    import pydoc
+    print(f"{file}:")
+    if not os.path.isfile(file):
+        raise ValueError(f"{file}: no such file")
+    pydoc.pager(open(file).read())
