da3e12fab5f233f53cb97d289a5569a206e9bea4
Tim Fuller <tjfulle@sandia.gov>
2025-05-29 11:43:31 -0600

    update unit test config fixture to start from a clean env each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,5 +11,17 @@ import _canary.config
 
 @pytest.fixture(scope="function", autouse=True)
 def config():
-    os.environ["CANARY_DISABLE_KB"] = "1"
-    _canary.config._config = _canary.config.config.Config.factory()
+    try:
+        env_copy = os.environ.copy()
+        os.environ.pop("CANARYCFG64", None)
+        os.environ["CANARY_DISABLE_KB"] = "1"
+        _canary.config._config = _canary.config.config.Config.factory()
+        _canary.config._config.resource_pool.fill_uniform(
+            node_count=1,
+            cpus_per_node=8,
+            gpus_per_node=0,
+        )
+        yield
+    except:
+        os.environ.clear()
+        os.environ.update(env_copy)
