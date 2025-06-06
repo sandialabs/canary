2e69e0e7d2fe64093f62de808d68204f63b0d136
Tim Fuller <tjfulle@sandia.gov>
2025-06-06 08:50:43 -0600

    fix show-capture flag to display o and e separately

--- src/_canary/plugins/builtin/show_capture.py
+++ src/_canary/plugins/builtin/show_capture.py
@@ -24,15 +24,20 @@ def canary_addoption(parser: "Parser") -> None:
         "--show-capture",
         command="run",
         group="console reporting",
-        action="store_true",
-        help="Show captured stdout and stderr for failed tests [default: %(default)s]",
+        nargs="?",
+        choices=("o", "e", "oe", "no"),
+        default="no",
+        const="oe",
+        help="Show captured stdout (o), stderr (e), or both (oe) "
+        "for failed tests [default: %(default)s]",
     )
     parser.add_argument(
         "--capture",
         command="run",
         choices=("log", "tee"),
         default="log",
-        help="Log test output to a file only (default) or log and print output "
+        group="console reporting",
+        help="Log test output to a file only (log) or log and print output "
         "to the screen (tee).  Warning: this could result in a large amount of text printed "
         "to the screen [default: log]",
     )
@@ -40,21 +45,23 @@ def canary_addoption(parser: "Parser") -> None:
 
 @hookimpl(trylast=True)
 def canary_session_finish(session: "Session", exitstatus: int) -> None:
-    if config.getoption("show_capture"):
-        cases = session.active_cases()
-        failed = [
-            case for case in cases if not case.status.satisfies(("success", "xdiff", "xfail"))
-        ]
-        if failed:
-            _, width = terminal_size()
-            print(
-                ccenter(colorize(" @*R{%d Test failures} " % len(failed)), width, "="), end="\n\n"
-            )
-            for case in failed:
-                show_capture(case)
+    what = config.getoption("show_capture")
+    if what in ("no", None):
+        return
+    cases = session.active_cases()
+    failed = [
+        case for case in cases if not case.status.satisfies(("success", "xdiff", "xfail"))
+    ]
+    if failed:
+        _, width = terminal_size()
+        print(
+            ccenter(colorize(" @*R{%d Test failures} " % len(failed)), width, "="), end="\n\n"
+        )
+        for case in failed:
+            show_capture(case, what=what)
 
 
-def show_capture(case: "TestCase") -> None:
+def show_capture(case: "TestCase", what="oe") -> None:
     _, width = terminal_size()
     color = "g" if case.status == "success" else "R" if case.status == "failed" else "y"
     fp = io.StringIO()
@@ -62,21 +69,21 @@ def show_capture(case: "TestCase") -> None:
     fp.write(f"{bold('Status')}: {case.status.cname}\n")
     fp.write(f"{bold('Execution directory')}: {case.execution_directory}\n")
     fp.write(f"{bold('Command')}: {' '.join(case.command())}\n")
-    if case.stdout_file:
+    if what in ("o", "oe") and case.stdout_file:
         file = case.stdout_file
         if os.path.exists(file):
             with open(file) as fh:
                 stdout = fh.read().strip()
             if stdout:
-                fp.write(bold("Captured stdout") + "\n")
+                fp.write(bold("stdout") + "\n")
                 fp.write(indent(stdout, "  ") + "\n")
-    if case.stderr_file:
+    if what in ("e", "oe") and case.stderr_file:
         file = case.stderr_file
         if os.path.exists(file):
             with open(file) as fh:
                 stderr = fh.read().strip()
             if stderr:
-                fp.write(bold("Captured stderr") + "\n")
+                fp.write(bold("stderr") + "\n")
                 fp.write(indent(stderr, "  ") + "\n")
     text = fp.getvalue()
     if text.strip():
