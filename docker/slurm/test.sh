0bf5ce739305907aab38d9837c92c066bf7a5a3d
Alex Knigge <amknigg@sandia.gov>
2025-06-30 17:24:02 -0600

    putting tests into one script

---
+++ docker/slurm/test.sh
@@ -0,0 +1,26 @@
+source /canary/bin/activate
+canary fetch examples
+
+# Test 1
+exit_code=0
+canary run -w -b scheduler=slurm ./examples || exit_code=$?
+if [ "${exit_code}" -ne 30 ]; then exit 1; fi
+
+# Test 2
+exit_code=0
+canary run -w -b scheduler=slurm -b spec=count:3 ./examples || exit_code=$?
+if [ "${exit_code}" -ne 30 ]; then exit 1; fi
+
+# Test 3
+exit_code=0
+canary run -w -b scheduler=slurm -b spec=count:3,layout:atomic ./examples || exit_code=$?
+if [ "${exit_code}" -ne 30 ]; then exit 1; fi
+
+# Test 4
+exit_code=0
+canary run -w -b scheduler=slurm -b spec=count:auto,layout:flat ./examples || exit_code=$?
+if [ "${exit_code}" -ne 30 ]; then exit 1; fi
+
+# Artifacts
+canary -C TestResults report junit create -o $CI_PROJECT_DIR/junit.xml || true
+canary -C TestResults report cdash create -d $CI_PROJECT_DIR/xml || true
