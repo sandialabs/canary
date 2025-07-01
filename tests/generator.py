d0f52c755e4e137621840387d4a9118e100428a8
Alex Knigge <amknigg@sandia.gov>
2025-07-01 16:34:19 -0600

    adding an option for cpus per node

--- tests/generator.py
+++ tests/generator.py
@@ -18,13 +18,13 @@ canary.directives.name('baz')
 canary.directives.analyze()
 canary.directives.owner('me')
 canary.directives.keywords('test', 'unit')
-canary.directives.parameterize('cpus', (1, 2), when="options='baz'")
+canary.directives.parameterize('cpus', (1, 2, 3), when="options='baz'")
 canary.directives.parameterize('a,b,c', [(1, 11, 111), (2, 22, 222), (3, 33, 333)])
 """
             )
         file = pyt.PYTTestGenerator(".", "test.pyt")
         cases = file.lock(on_options=["baz"])
-        assert len(cases) == 7
+        assert len(cases) == 10
         assert isinstance(cases[-1], tc.TestMultiCase)
 
 
@@ -36,11 +36,11 @@ def test_vvt_generator(tmpdir):
 # VVT: name: baz
 # VVT: analyze : --analyze
 # VVT: keywords: test unit
-# VVT: parameterize (options=baz) : np=1 2
+# VVT: parameterize (options=baz) : np=1 2 3
 # VVT: parameterize : a,b,c=1,11,111 2,22,222 3,33,333
 """
             )
         file = vvt.VVTTestGenerator(".", "test.vvt")
         cases = file.lock(on_options=["baz"])
-        assert len(cases) == 7
+        assert len(cases) == 10
         assert isinstance(cases[-1], tc.TestMultiCase)
