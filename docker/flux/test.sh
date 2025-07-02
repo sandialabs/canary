356a6506380a6efefd1c3e168f6aa5bc50403720
Alex Knigge <amknigg@sandia.gov>
2025-07-02 13:48:47 -0600

    removing docker notation from test script

--- docker/flux/test.sh
+++ docker/flux/test.sh
@@ -1,4 +1,4 @@
-RUN python3 -m venv canary && \
-    . canary/bin/activate && \
-    python -m pip install --upgrade pip==25.1.1 && \
-    python3 -m pip install "canary-wm@git+https://git@github.com/sandialabs/canary@$BRANCH_NAME"
+python3 -m venv canary && \
+. canary/bin/activate && \
+python -m pip install --upgrade pip==25.1.1 && \
+python3 -m pip install "canary-wm@git+https://git@github.com/sandialabs/canary@$BRANCH_NAME"
