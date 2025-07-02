8c23bfbe87e866506ad9e3fcd72f19d35dcc24a3
Alex Knigge <amknigg@sandia.gov>
2025-07-02 15:54:37 -0600

    changing from prebuilt flux container to my own container

--- docker/flux/test.sh
+++ docker/flux/test.sh
@@ -1,4 +1,26 @@
-python3 -m venv canary && \
-. canary/bin/activate && \
-python -m pip install --upgrade pip==25.1.1 && \
-python3 -m pip install "canary-wm@git+https://git@github.com/sandialabs/canary@$BRANCH_NAME"
+source /canary/bin/activate
+canary fetch examples
+
+# Test 1
+exit_code=0
+canary run -w -b scheduler=flux ./examples || exit_code=$?
+if [ "${exit_code}" -ne 30 ]; then exit 1; fi
+
+# Test 2
+exit_code=0
+canary run -w -b scheduler=flux -b spec=count:3 ./examples || exit_code=$?
+if [ "${exit_code}" -ne 30 ]; then exit 1; fi
+
+# Test 3
+exit_code=0
+canary run -w -b scheduler=flux -b spec=count:3,layout:atomic ./examples || exit_code=$?
+if [ "${exit_code}" -ne 30 ]; then exit 1; fi
+
+# Test 4
+exit_code=0
+canary run -w -b scheduler=flux -b spec=count:auto,layout:flat ./examples || exit_code=$?
+if [ "${exit_code}" -ne 30 ]; then exit 1; fi
+
+# Artifacts
+canary -C TestResults report junit create -o $CI_PROJECT_DIR/junit.xml || true
+canary -C TestResults report cdash create -d $CI_PROJECT_DIR/xml || true
