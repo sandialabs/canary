b8492f81939a268327e0182247b724c47909e92f
Alex Knigge <amknigg@sandia.gov>
2025-07-02 13:47:32 -0600

    trying canary install inside flux container

--- docker/flux/test.sh
+++ docker/flux/test.sh
@@ -1,3 +1,4 @@
-python3 --help
-echo "Inside test script"
-
+RUN python3 -m venv canary && \
+    . canary/bin/activate && \
+    python -m pip install --upgrade pip==25.1.1 && \
+    python3 -m pip install "canary-wm@git+https://git@github.com/sandialabs/canary@$BRANCH_NAME"
