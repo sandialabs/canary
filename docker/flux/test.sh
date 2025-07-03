b2a65e20511f0fa48bd9c63aab84bb7049bafc10
Alex Knigge <amknigg@sandia.gov>
2025-07-03 15:31:29 -0600

    adding extra pretty text

--- docker/flux/test.sh
+++ docker/flux/test.sh
@@ -16,36 +16,50 @@ canary fetch examples
 # Install flux-python plugin
 python3 -m pip install flux-python
 
-echo
-echo
-echo
-echo
+echo " "
+echo " "
+echo " "
+echo " "
 echo "----------------------------------------------------"
-echo "Starting flux tests..."
-echo
-echo
-echo
+echo "Starting Flux tests..."
+echo " "
+echo " "
+echo " "
 
+echo "------------------------Test 1----------------------"
+echo " "
 # Test 1
 exit_code=0
 canary run -w -b scheduler=flux ./examples || exit_code=$?
 if [ "${exit_code}" -ne 30 ]; then exit 1; fi
 
+echo " "
+echo "------------------------Test 2----------------------"
+echo " "
 # Test 2
 exit_code=0
 canary run -w -b scheduler=flux -b spec=count:3 ./examples || exit_code=$?
 if [ "${exit_code}" -ne 30 ]; then exit 1; fi
 
+echo " "
+echo "------------------------Test 3----------------------"
+echo " "
 # Test 3
 exit_code=0
 canary run -w -b scheduler=flux -b spec=count:3,layout:atomic ./examples || exit_code=$?
 if [ "${exit_code}" -ne 30 ]; then exit 1; fi
 
+echo " "
+echo "------------------------Test 4----------------------"
+echo " "
 # Test 4
 exit_code=0
 canary run -w -b scheduler=flux -b spec=count:auto,layout:flat ./examples || exit_code=$?
 if [ "${exit_code}" -ne 30 ]; then exit 1; fi
 
+echo " "
+echo " "
+echo "----------------------- Done! ----------------------"
 # Artifacts
 canary -C TestResults report junit create -o $CI_PROJECT_DIR/junit.xml || true
 canary -C TestResults report cdash create -d $CI_PROJECT_DIR/xml || true
