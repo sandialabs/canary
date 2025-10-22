92819741f8cf90a55eb7dc9d1a72bb304d4d116f
Tim Fuller <tjfulle@sandia.gov>
2025-10-22 10:58:03 -0600

    adding a meaningful config.resource_pool attribute error

--- src/_canary/config/__init__.py
+++ src/_canary/config/__init__.py
@@ -11,6 +11,28 @@ from .config import Config
 from .config import ConfigScope  # noqa: F401
 from .config import get_scope_filename  # noqa: F401
 
+
+class _resource_pool_attr_error:
+    def __getattribute__(self, name):
+        import io
+
+        f = io.StringIO()
+        f.write(
+            "The global canary resource pool has been removed in favor of executor-specific "
+            "resource pools.  Properties of the resource pool are accessed through the canary "
+            "plugin manager. "
+        )
+        if name in ("count", "types", "accommodates"):
+            repl = f"config.pluginmanger.canary_resource_pool_{name}"
+        else:
+            repl = f"a plugin call that can return the pool's {name!r} attribute"
+        f.write(f"In this case, replace config.resource_pool.{name} with {repl}.")
+        raise AttributeError(f.getvalue().strip()) from None
+
+
+resource_pool = _resource_pool_attr_error()
+
+
 if TYPE_CHECKING:
     _config = Config()
 
