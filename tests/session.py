1749771a9ee491dcf876442c972b7e4176cc48c8
Matthew Mosby <mdmosby@sandia.gov>
2025-10-29 08:37:12 -0600

    patch the resource detection to support tests on small runners

--- tests/session.py
+++ tests/session.py
@@ -4,6 +4,9 @@
 
 import os
 import types
+from unittest.mock import patch
+
+import psutil
 
 import _canary.config as config
 import _canary.session as session
@@ -21,31 +24,34 @@ def paths():
 
 def test_session_filter(tmpdir):
     p = paths()
-    with working_dir(tmpdir.strpath, create=True):
-        s = session.Session("tests", mode="w", force=True)
-        s.add_search_paths([os.path.join(p.examples, "basic"), os.path.join(p.examples, "vvt")])
-        s.discover()
-        s.lock()
-        s.run()
+    with patch("psutil.cpu_count", return_value=12):
+        with working_dir(tmpdir.strpath, create=True):
+            s = session.Session("tests", mode="w", force=True)
+            s.add_search_paths(
+                [os.path.join(p.examples, "basic"), os.path.join(p.examples, "vvt")]
+            )
+            s.discover()
+            s.lock()
+            s.run()
 
-        with working_dir("tests"):
-            s = session.Session(".", mode="r")
-            s.filter(keyword_exprs=["first"])
+            with working_dir("tests"):
+                s = session.Session(".", mode="r")
+                s.filter(keyword_exprs=["first"])
 
-        with working_dir("tests"):
-            s = session.Session(".", mode="r")
-            s.filter(parameter_expr="np=4")
-            cases = s.get_ready()
-            assert len(cases) == 1
-            assert cases[0].family == "test_exec_dir"
-            p = cases[0].path
+            with working_dir("tests"):
+                s = session.Session(".", mode="r")
+                s.filter(parameter_expr="np=4")
+                cases = s.get_ready()
+                assert len(cases) == 1
+                assert cases[0].family == "test_exec_dir"
+                p = cases[0].path
 
-        with working_dir(f"tests/{p}"):
-            s = session.Session(".", mode="r")
-            s.filter(start=os.getcwd())
-            cases = s.get_ready()
-            assert len(cases) == 1
-            assert cases[0].name == "test_exec_dir.np=4.x=1.234e7"
+            with working_dir(f"tests/{p}"):
+                s = session.Session(".", mode="r")
+                s.filter(start=os.getcwd())
+                cases = s.get_ready()
+                assert len(cases) == 1
+                assert cases[0].name == "test_exec_dir.np=4.x=1.234e7"
 
 
 def test_session_fail_fast(tmpdir):
