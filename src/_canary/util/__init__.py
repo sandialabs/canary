c82f5dc58b0a2c17503606ebf755f0f301aecb58
Matthew Mosby <mdmosby@sandia.gov>
2025-10-29 13:03:23 -0600

    improve typing/error checking in `cpu_count`

--- src/_canary/util/__init__.py
+++ src/_canary/util/__init__.py
@@ -6,9 +6,12 @@
 import psutil
 
 
-def cpu_count(logical: bool | None = None) -> int | None:
+def cpu_count(logical: bool | None = None) -> int:
     from .. import config  # lazy import to avoid circular deps
 
     if logical is None:
         logical = config.getoption("resource_pool_enable_hyperthreads", False)
-    return psutil.cpu_count(logical=logical)
+    count = psutil.cpu_count(logical=logical)
+    if count is None:
+        raise RuntimeError("Unable to determine the number of CPUs")
+    return count
