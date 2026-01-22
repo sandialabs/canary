6dce0dace259811b3b1657bd1f0aa1b68cb29a8b
Tim Fuller <tjfulle@sandia.gov>
2026-01-22 12:50:17 -0700

    adding canary select --from

--- src/_canary/plugins/subcommands/select.py
+++ src/_canary/plugins/subcommands/select.py
@@ -33,6 +33,9 @@ class Select(CanarySubcommand):
         )
         group.add_argument("-d", dest="delete_tag", action="store_true", help="Delete tag")
         group.add_argument("-m", dest="move_tag", metavar="oldtag", help="Move/rename a tag")
+        group.add_argument(
+            "--from", dest="from_tag", metavar="tag", help="Create selection from tag"
+        )
         Selector.setup_parser(parser)
 
     def execute(self, args: "argparse.Namespace") -> int:
@@ -41,6 +44,25 @@ class Select(CanarySubcommand):
             workspace.db.delete_selection(args.tag)
         elif args.move_tag:
             workspace.db.rename_selection(args.move_tag, args.tag)
+        elif args.from_tag:
+            resolved = workspace.db.load_specs_by_tagname(args.from_tag)
+            specs = workspace.select_from_specs(
+                resolved,
+                prefixes=args.start_dir,
+                keyword_exprs=args.keyword_exprs,
+                parameter_expr=args.parameter_expr,
+                owners=args.owners,
+                regex=args.regex_filter,
+            )
+            workspace.db.put_selection(
+                args.tag,
+                specs,
+                prefixes=args.start_dir,
+                keyword_exprs=args.keyword_exprs,
+                parameter_expr=args.parameter_expr,
+                owners=args.owners,
+                regex=args.regex_filter,
+            )
         else:
             if workspace.is_tag(args.tag):
                 raise ValueError(logging.colorize(f"Selection {args.tag!r} already exists"))
