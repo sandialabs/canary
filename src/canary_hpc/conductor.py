977d6fe9bee7a990fac9df4730d8992c5cf2c4fa
Matthew Mosby <mdmosby@sandia.gov>
2026-08-06 13:51:06 -0600

    fix option value comparison

--- src/canary_hpc/conductor.py
+++ src/canary_hpc/conductor.py
@@ -57,7 +57,7 @@ class CanaryHPCConductor:
         pluginmanager.register(self, "canary_hpc_conductor")
 
     def run(self, args: argparse.Namespace) -> int:
-        if n := args.hpc_batch_workers:
+        if n := int(args.hpc_batch_workers):
             if n > cpu_count():
                 logger.warning(f"--hpc-batch-workers={n} > cpu_count={cpu_count()}")
         batchspec = args.hpc_batchspec or CanaryHPCBatchSpec.defaults()
