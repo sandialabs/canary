6eac5abaa2be1c91c29f3c49f0e730b32addbf5a
Tim Fuller <tjfulle@sandia.gov>
2026-09-10 19:52:44 -0600

    Update test.sh

--- .ci/slurm/test.sh
+++ .ci/slurm/test.sh
@@ -19,61 +19,15 @@ echo " "
 echo " "
 echo " "
 
+echo " "
 echo "------------------------Test 1----------------------"
-echo " "
-# Test 1
-exit_code=0
-canary -d run --show-excluded-tests -w -b scheduler=slurm ./examples || exit_code=$?
-if [ "${exit_code}" -ne 14 ]; then
-  cat .canary/cache/canary-hpc/batches/*/resource_pool.json || true
-  cat .canary/cache/canary-hpc/batches/*/canary-out.txt || true
-  cat TestResults/basic/second/second/canary-out.txt || true
-  cat TestResults/basic/second/second/canary-err.txt || true
-  exit 1
-fi
-
-echo " "
-echo "------------------------Test 2----------------------"
 echo " "
 # Test 2
 exit_code=0
 canary -d run --show-excluded-tests -w -b scheduler=slurm -b spec=count:3,nodes:any ./examples || exit_code=$?
 if [ "${exit_code}" -ne 14 ]; then
-  cat .canary/cache/canary-hpc/batches/*/resource_pool.json || true
-  cat .canary/cache/canary-hpc/batches/*/canary-out.txt || true
-  cat TestResults/basic/second/second/canary-out.txt || true
-  cat TestResults/basic/second/second/canary-err.txt || true
   exit 1
 fi
-
-echo " "
-echo "------------------------Test 3----------------------"
-echo " "
-# Test 3
-exit_code=0
-canary -d run --show-excluded-tests -w -b scheduler=slurm -b spec=count:3,layout:atomic,nodes:any ./examples || exit_code=$?
-if [ "${exit_code}" -ne 14 ]; then
-  cat .canary/cache/canary-hpc/batches/*/resource_pool.json || true
-  cat .canary/cache/canary-hpc/batches/*/canary-out.txt || true
-  cat TestResults/basic/second/second/canary-out.txt || true
-  cat TestResults/basic/second/second/canary-err.txt || true
-  exit 1
-fi
-
-echo " "
-echo "------------------------Test 4----------------------"
-echo " "
-# Test 4
-exit_code=0
-canary -d run --show-excluded-tests -w -b scheduler=slurm -b spec=duration:2m,layout:flat,nodes:any ./examples || exit_code=$?
-if [ "${exit_code}" -ne 14 ]; then
-  cat .canary/cache/canary-hpc/batches/*/resource_pool.json || true
-  cat .canary/cache/canary-hpc/batches/*/canary-out.txt || true
-  cat TestResults/basic/second/second/canary-out.txt || true
-  cat TestResults/basic/second/second/canary-err.txt || true
-  exit 1
-fi
-
 
 echo " "
 echo " "
